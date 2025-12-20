941c191078174770b22c28dab942270407a2dc2a
SirAlmiraj <danialomar24@gmail.com>
2025-12-20 14:56:49 +0000

    changed dict init

--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ def count_words(book):
     return len(word_count)
 
 def unique_char_count(book):
-    unique_char_dict = {str:int}
+    unique_char_dict = {}
     book = book.lower()
     for i in book:
         if i in unique_char_dict:
